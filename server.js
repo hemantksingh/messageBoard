b254816c2be1b91f555202b8dd925b9f39a3feeb
Hemant <hemantksingh.hk@gmail.com>
2014-07-15 02:15:37 +0100

    Enabled rendering of simple web page using vash and jade view engines.

---
+++ server.js
@@ -0,0 +1,30 @@
+var http = require("http");
+var express = require("express");
+/*var server = http.createServer(function(req, res) {
+	console.log(req.url);
+	res.write("<html><body><h1>" + req.url +"</h1></body></html>");
+
+	res.end();
+});*/
+var app = express();
+//console.log(__dirname + '\\views');
+//app.set("view engine", "jade");
+app.set("view engine", "vash");
+
+app.get("/", function(req, res){
+	//res.send("<html><body><h1>" + "Express!!!" +"</h1></body></html>")
+
+	// By default the views folder is looked up for the view.
+	// res.render("jade/index", {title: "Express with Jade"});
+	res.render("index", {title: "Express with vash"});
+});
+
+app.get("/api/users", function(req, res) {
+	res.set("Content-Type", "application/json");
+	res.send({name: "Shawn", isValid: true, group: "Admin"});
+})
+var server = http.createServer(app);
+
+// port 80 is a public facing web server.
+server.listen(3000);
+console.log("Server started at port:3000");
