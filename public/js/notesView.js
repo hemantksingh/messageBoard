e05d9ae602cdd6e750a4296254af7dc0f7b20053
Hemant <hemantksingh.hk@gmail.com>
2014-07-28 01:58:38 +0100

    Enabled adding of notes to categories.

--- public/js/notesView.js
+++ public/js/notesView.js
@@ -1,11 +1,13 @@
 (function (angular) {
-	var app = angular.module("notesView", []);
-	app.controller("notesViewController", 
-		["$scope", "$window", "$http", 
+	var app = angular.module("notesView", ["ui.bootstrap"]);
+	app.controller("notesViewController", [
+		"$scope", "$window", "$http", 
 			function($scope, $window, $http) {		
 				var urlParts = $window.location.pathname.split("/");
 				var categoryName = urlParts[urlParts.length - 1];
 				var notesUrl = "/api/notes/" + categoryName;
+
+				$scope.newNote = createNote();
 
 				$http.get(notesUrl)
 					.then(function(result) {
@@ -13,6 +15,25 @@
 					}, function(err) {
 						alert("Failed to retreive notes.");
 					});
+
+				$scope.save = function() {
+					$http.post(notesUrl, $scope.newNote)
+						.then(function(result) {
+							$scope.notes.push(result.data);
+							$scope.newNote = createNote();
+						}, function(err){
+							alert('Failed to create the note.');
+						});
+				};
 			}
-		]);
+	]);
+
+	function createNote() {
+		return {
+			note: "",
+			color: "yellow",
+			author: "Hemant Kumar"
+		};
+	}
+
 })(window.angular);
